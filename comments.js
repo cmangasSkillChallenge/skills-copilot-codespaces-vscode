d4722869af57855111b995dcded7e5d33a70c0cd
cmangasl <163734450+cmangasl@users.noreply.github.com>
2024-03-17 13:29:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,28 @@
-// crea un servidor web
-// para pruebas en local
-// usando la libreria express
-const express = require('express');
-const app = express();
-const port = 3000;
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+var comments = require('./comments.json');
 
-app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/comments', (req, res) => {
-  res.json([
-    {
-      id: 0,
+app.get('/comments', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(comments));
+});
+
+app.post('/comments', function(req, res) {
+	var comment = req.body;
+	comments.push(comment);
+	fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify(comments));
+	});
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
